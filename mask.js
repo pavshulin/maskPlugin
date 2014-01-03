903261aeafd5701bf6d9827436d7f8f80d43e0a1
pavshulin <pavshulin@gmail.com>
2014-01-03 11:55:26 +0200

    fix change event when isFocused is false

--- mask.js
+++ mask.js
@@ -280,7 +280,7 @@
     };
 
 
-    function _onFocus (event) {
+    function _onFocus () {
         this.isFocused = true;
 
         setTimeout(this.focusNavigate, 0);
@@ -368,6 +368,18 @@
             enteredSymbols = newText.slice(start, start + difference),
             method = 'addingText',
             position;
+
+        if (!this.isFocused) {
+            this.removeText(newText);
+            this.writeDown();
+
+            if (!this.isEntered || (this.clearIncomplete
+                && this.lastSign < this.lastSymbol)) {
+                this.clearUp();
+            }
+
+            return;
+        }
 
         if (this._isComplete && carr.begin === carr.end && difference > 0){
             this.writeDown();
