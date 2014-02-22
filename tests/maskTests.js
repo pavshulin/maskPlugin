106e68e53e93d198b87724a937ec9836c74cb113
pavshulin@gmail.com <pavshulin@gmail.com>
2014-02-22 16:54:00 +0200

    add unit tests

--- tests/maskTests.js
+++ tests/maskTests.js
@@ -14,6 +14,22 @@
 
             this.input.maskPlugin(mask, params)
             this.mask = this.input.data('maskPlugin');    
+
+            this.moveCarr = function (begin, el, end) {
+                end = end || begin;
+                el = el || this.input;
+                el.each(function () {
+                    this.setSelectionRange(begin, end);
+                });
+            };
+
+            this.getCarr = function (el) {
+                el = el || this.input;
+                return {
+                    begin: el[0].selectionStart,
+                    end: el[0].selectionEnd
+                };
+            };
         }
 
         Function.prototype.bind = Function.prototype.bind || function (obj) {
@@ -35,6 +51,8 @@
         }
 
         delete this.maskCreate;
+        delete this.getCarr;
+        delete this.moveCarr;
     }    
 
     module('Mask Plugin Test', {
@@ -189,8 +207,28 @@
         equal(this.input.val(), 'a1321 - d1441 - 34323');
     });
 
-    test( "reset function test", function () {
 
+    test( "destroy function test", function () {
+        this.maskCreate();
+
+        this.mask.destroy();
+
+        this.mask = this.input.data('maskPlugin');
+
+        ok(!this.mask, 'mask was not destroyed');
+        ok(!this.input.hasClass('maskPlugin'), 'mask was not destroyed');
+
+        this.input.change().trigger('blur');
+
+        equal(this.input.val(), '', 'mask was not destroyed');
+
+        this.input.val('1234').trigger('input');
+        equal(this.input.val(), '1234', 'mask was not destroyed');
+
+        delete this.mask;
+    });
+
+    test( "reset function test", function () {
         this.maskCreate();
 
         this.input.val('a2345a789012345').trigger('input');
@@ -199,20 +237,93 @@
             'reset function work wrong');
 
         this.mask.reset('9999 - a - 9999');
+        this.mask = this.input.data('maskPlugin');
+
         equal(this.input.val(), '2345 - a - 7890', 
             'reset function work wrong');
+    });3
 
-        delete this.mask
-    });
-    
-    test( "reset function test", function () {
+    test('carriage position', function () {
+        var position = {},
+            _event;
         this.maskCreate();
 
-        this.input.val('a2345a789012345').trigger('input');
-        ok(true);
+        this.moveCarr(0);
+        this.input.trigger('focus');
+
+        position = {
+            begin:0,
+            end:0
+         };
+
+        deepEqual(this.getCarr(), position, 'carriage works wrong');
+
+        this.moveCarr(5);
+        this.input.trigger('focus');
+        deepEqual(this.getCarr(), position, 'carriage works wrong');
+
+        this.moveCarr(6);
+        this.input.trigger('keyup');
+
+        deepEqual(this.getCarr(), position, 'carriage works wrong');
+
+
+        this.moveCarr(6);
+        this.input.trigger('mouseup');
+
+        deepEqual(this.getCarr(), position, 'carriage works wrong');    
+
+        this.mask.reset(' (99999 9999) ', {
+            allwaysMask: true   
+        });
+
+        this.mask = this.input.data('maskPlugin');
+
+        position = {
+            begin: 2,
+            end: 2
+        };
+
+        this.input.trigger('focus').trigger('mousedown');
+        this.moveCarr(6);
+        this.input.trigger('mouseup');
+        
+        deepEqual(this.getCarr(), position, 'carriage works wrong');
+
+        _event = jQuery.Event( "keyup", { keyCode: 39 } );
+        _event.which = 39;
+        
+        this.input.trigger('focus').trigger('keydown');
+        this.moveCarr(6);
+        this.input.trigger(_event);
+        
+        deepEqual(this.getCarr(), position, 'carriage works wrong');
+
+        _event = jQuery.Event( "keyup", { keyCode: 40 } );
+        _event.which = 40;
+        
+        this.input.trigger('focus').trigger('keydown');
+        this.moveCarr(13);
+        this.input.trigger(_event);
+        
+        deepEqual(this.getCarr(), position, 'carriage works wrong');
+
+        this.input.val('1234').trigger('input');
+        _event = jQuery.Event( "keyup", { keyCode: 40 } );
+        _event.which = 40;
+        
+        this.input.trigger('focus').trigger('keydown');
+        this.moveCarr(13);
+        this.input.trigger(_event);
+        
+        position = {
+            begin: 6,
+            end: 6
+        };
+
+        deepEqual(this.getCarr(), position, 'carriage works wrong');
 
 
     });
-
 
 } ());
